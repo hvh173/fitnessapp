c141ea952cfea9e4d64b99edd0e7b6298abf51ba
hvh173 <hoanghazoro321@gmail.com>
2025-12-13 10:47:31 +0700

    Delete lib directory

--- lib/lib/constants/app_colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color primary = Color.fromARGB(255, 22, 109, 185);
-  static const Color accent = Color(0xFFFF4081);  
-  static const Color background = Color(0xFFF5F5F5);
-  static const Color cardColor = Colors.white;
-}
